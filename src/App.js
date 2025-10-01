c06a3b1c05f756c915126143540a4ad835e9be09
Mohamed Hany <MohamedEgy246@gmail.com>
2025-10-01 15:33:32 +0300

    update status when all items are packed.

--- src/App.js
+++ src/App.js
@@ -154,15 +154,18 @@ function Item({ item, onPackItems, onDel }) {
 }
 function Stats({ items }) {
   const packed = items.filter((item) => item.packed).length;
+  const percentage = Math.round((packed / items.length) * 100);
   return (
     <footer className="stats">
-      {items.length ? (
+      {!items.length ? (
+        <em>Start adding items to the packing list</em>
+      ) : percentage == 100 ? (
+        "You Are Ready To Go!"
+      ) : (
         <em>
           You have {items.length} itmes on your list and you are already packed{" "}
-          {packed} ({((packed / items.length) * 100).toFixed(0)}%)
+          {packed} ({percentage}%)
         </em>
-      ) : (
-        <em>Start adding items to the packing list</em>
       )}
     </footer>
   );
